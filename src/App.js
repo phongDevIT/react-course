791ffc781b07bc059549c78f81101be37737ece9
nemo123 <nemo24112003357@gmail.com>
2023-03-10 23:09:45 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import "./App.css";
 import Card from "./component/card/Card";
+import Card2 from "./component/card/Card2";
 import CardList from "./component/card/CardList";
 import { GlobalStyles } from "./GlobalStyles";
-
+import { ThemeProvider } from "styled-components";
+const ThemeStyle = {
+    colors: {
+        blue: "#2979ff",
+        red: "#d42222",
+    },
+    orange: "#ffa400",
+};
 function App() {
     return (
-        <div>
+        <ThemeProvider theme={ThemeStyle}>
             <GlobalStyles></GlobalStyles>
-            {/* <YoutubeList></YoutubeList> */}
-            {/* <Toggle></Toggle>
-            <Counter></Counter> */}
-            {/* <Game></Game> */}
-            {/* <Button>Primary</Button>
-            <Button secondary>Secondary</Button> */}
             <CardList>
-                <Card primary={true}></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
+                <Card2></Card2>
             </CardList>
-        </div>
+        </ThemeProvider>
     );
 }
 
---
+++ src/component/card/Card2.js
@@ -0,0 +1,156 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+const StyleCard = styled.div`
+    position: relative;
+    margin: 30px;
+    .card-image {
+        height: 400px;
+        width: 400px;
+        border-radius: 8px;
+        img {
+            width: 100%;
+            height: 100%;
+            display: block;
+            border-radius: inherit;
+            object-fit: cover;
+        }
+    }
+`;
+
+// const StyleCard = styled.div`
+//     position: relative;
+
+//     margin: 30px;
+// `;
+const CartImage = styled.div`
+    height: 400px;
+    width: 400px;
+    border-radius: 8px;
+`;
+const CardImg = styled.img`
+    width: 100%;
+    height: 100%;
+    display: block;
+    border-radius: inherit;
+    object-fit: cover;
+`;
+const CardContent = styled.div`
+    position: absolute;
+    background-color: #fff;
+    width: calc(100%, -36px);
+    padding: 14px;
+    left: 36%;
+    transform: translate(-50%, 50%);
+    border-radius: 20px;
+    bottom: 0;
+    z-index: 10;
+`;
+const CardTop = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 30px;
+`;
+const CartUser = styled.div`
+    display: flex;
+    align-items: center;
+    column-gap: 20px;
+`;
+const UserName = styled.span`
+    font-weight: 300;
+    font-size: 16px;
+    color: ${(props) => props.theme.colors.red}; ;
+`;
+const CardUserImg = styled.img`
+    height: 30px;
+    width: 30px;
+    border-radius: 100rem;
+    object-fit: cover;
+    flex-shrink: 0;
+`;
+const CardIcon = styled.div`
+    display: flex;
+    align-items: center;
+    column-gap: 12px;
+`;
+const CardText = styled.span`
+    color: ${(props) => props.theme.colors.blue};
+    font-weight: 400;
+    font-size: 16px;
+`;
+const CardFooter = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+const CardTitle = styled.h3`
+    width: 230px;
+    height: 27px;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 27px;
+    color: black;
+    ${(props) =>
+        props.secondary &&
+        css`
+            color: #20e2b2;
+        `};
+
+    -webkit-background-clip: text;
+    background-clip: text;
+`;
+const CardSNumber = styled.span`
+    font-size: 18px;
+    font-weight: bold;
+    color: transparent;
+    background: linear-gradient(
+        86.88deg,
+        #7d6aff 1.38%,
+        #ffb86c 64.35%,
+        #fc2872 119.91%
+    );
+    ${(props) =>
+        props.primary &&
+        css`
+            background: linear-gradient(86.88deg, #20e2b2 #2cccff);
+        `};
+    -webkit-background-clip: text;
+    background-clip: text;
+`;
+const Card2 = (props) => {
+    console.log("props: ", props);
+    return (
+        <StyleCard>
+            <div className="card-image">
+                <img
+                    src="https://cdn.dribbble.com/users/2400293/screenshots/16630840/media/ed6af0d38d460ff9ee426ad69e6435ea.png?compress=1&resize=1000x750&vertical=top"
+                    alt=""
+                />
+            </div>
+            <CardContent>
+                <CardTop>
+                    <CartUser>
+                        <CardUserImg
+                            src="https://cdn.dribbble.com/users/2400293/screenshots/16630840/media/ed6af0d38d460ff9ee426ad69e6435ea.png?compress=1&resize=1000x750&vertical=top"
+                            alt=""
+                        />
+                        <UserName>@zndrson</UserName>
+                    </CartUser>
+                    <CardIcon>
+                        <img src="/icon-heart.svg" alt="heart" />
+                        <CardText>256</CardText>
+                    </CardIcon>
+                </CardTop>
+                <CardFooter>
+                    <CardTitle secondary>Cosmic Perspective</CardTitle>
+                    <CardSNumber primary={props.primary}>
+                        12,000 PSL
+                    </CardSNumber>
+                </CardFooter>
+            </CardContent>
+        </StyleCard>
+    );
+};
+
+export default Card2;
