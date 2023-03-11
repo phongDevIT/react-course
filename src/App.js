20b205cea5995be2763c50c1d570fbf7d67e081f
nemo123 <nemo24112003357@gmail.com>
2023-03-11 18:50:10 +0700

    commit style code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import "./App.css";
-import Card from "./component/card/Card";
-import Card2 from "./component/card/Card2";
+import React from "react";
+// import "./App.css";
+import CardTailwind from "./component/card/CardTailwind";
 import CardList from "./component/card/CardList";
-import { GlobalStyles } from "./GlobalStyles";
-import { ThemeProvider } from "styled-components";
-const ThemeStyle = {
-    colors: {
-        blue: "#2979ff",
-        red: "#d42222",
-    },
-    orange: "#ffa400",
-};
 function App() {
     return (
-        <ThemeProvider theme={ThemeStyle}>
-            <GlobalStyles></GlobalStyles>
+        <div>
             <CardList>
-                <Card2></Card2>
+                <CardTailwind></CardTailwind>
             </CardList>
-        </ThemeProvider>
+        </div>
     );
 }
 
